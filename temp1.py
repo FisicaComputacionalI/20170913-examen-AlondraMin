a73f3bab36b37858a0f25d713f8cf6f31c2d338c
Isabel Pedraza <isapedraza@hotmail.com>
2018-01-09 01:33:43 +0100

    calificacion de examen

--- temp1.py
+++ temp1.py
@@ -2,6 +2,7 @@
 import matplotlib.pyplot as plt 
 import numpy as np
 
+#Esta definic'on no tiene las caraccter'isticas indicadas en el enunciado del examen. Una deficni'on m'as apropiada ser'ia : 2*np.cos(2*np.pi*t)+2015
 def f(t):
     return  2015 * np.cos(3*t)
 #Definimos el rango de una variables y el intervalo en el que cambian
